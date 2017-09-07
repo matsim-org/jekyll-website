a2fccdfa1caec5d3bf5ea9cae72c71c4eebeb297
Billy Charlton <charlton@tu-berlin.de>
2017-09-07 14:32:57 +0200

    fetch_blogposts.py: fetch latest posts via confluence API

---
+++ fetch_blogposts.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# Fetch latest MATSim blog posts via Confluence API
+
+import urllib.request, json, yaml
+
+num_posts_to_retrieve = 5
+
+latest_posts_url = ( "https://matsim.atlassian.net/wiki/rest/api/content/search?" + 
+                     "cql=type=blogpost%20order%20by%20created%20desc" + 
+                     "&limit=" + str(num_posts_to_retrieve) +
+                     "&expand=history,body.view")
+
+posts = []
+
+with urllib.request.urlopen(latest_posts_url) as url:
+    data = json.loads(url.read().decode())    
+    results = data['results']
+    base_url = data['_links']['base']
+        
+    for post in results:
+      item = {}
+      item['date'] = post['history']['createdDate'][:10]
+      item['title'] = post['title']
+      item['author'] = post['history']['createdBy']['displayName']
+      item['author_link'] = base_url + '/display/~' + post['history']['createdBy']['username']
+      item['link'] = base_url + post['_links']['webui']
+      item['body'] = post['body']['view']['value']
+
+      # todo: don't use default user image
+      image = post['history']['createdBy']['profilePicture']['path'];
+      item['image'] = 'https://matsim.atlassian.net' + image
+
+      posts.append(item)        
+      print(item['date'], item['title'])
+      print()
+
+with open('_data/news.yml','w') as outfile:
+    yaml.dump(posts, outfile, default_flow_style=False)      
+      
